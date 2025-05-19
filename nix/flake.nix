d3680b9668648e7fc6b37b889d9ce584b11aadbd
tatucosmin <cosminsebastian2004@gmail.com>
2025-05-19 21:56:44 +0300

    inital nix configuration

---
+++ nix/flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "Cosmin's nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+
+      nixpkgs.config.allowUnfree = true;
+      
+      environment.systemPackages =
+        [
+          pkgs.raycast
+          pkgs.bitwarden-desktop
+          pkgs.pyenv
+          pkgs.slack
+          pkgs.discord
+        ];
+
+      homebrew = {
+        enable = true;
+        # onActivation.cleanup = "uninstall";
+
+        taps = [ ];
+        brews = [ ];
+        casks = [ "font-zed-mono-nerd-font" "google-chrome" "docker" ];
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      system.primaryUser = "cosmin";
+
+      system.defaults = {
+        dock.autohide = true;
+        dock.show-recents = false;
+      };
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#mac
+    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.cosmin = ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ nix/home.nix
@@ -0,0 +1,98 @@
+{ lib, pkgs, ... }:
+
+{
+  home = {
+    stateVersion = "25.05";
+
+    homeDirectory = lib.mkForce "/Users/cosmin";
+    username = "cosmin";
+
+    # https://github.com/zupo/dotfiles/blob/main/flake.nix
+    file.".hushlogin" = {
+      text = "";
+    };
+
+    packages = with pkgs; [
+      zellij
+      starship
+      uv
+      ruff
+      gh
+      eza
+      
+      helix
+      nil
+    ];
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "tatucosmin";
+    userEmail = "cosminsebastian2004@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.zellij = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      theme = "tokyo-night-dark";
+    };
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "github_dark";
+    };
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+      }
+    ];
+  };
+  
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    shellAliases = {
+      zj = "zellij";
+      vim = "hx";
+      nvim = "hx";
+      ls = "eza";
+      ll = "eza -1";
+    };
+  };
+  
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+				normal = {
+					family = "ZedMono Nerd Font";
+					style = "Regular";
+				};
+				bold = {
+					family = "ZedMono Nerd Font";
+					style = "Bold";
+				};
+				italic = {
+					family = "ZedMono Nerd Font";
+					style = "Italic";
+				};
+				size = 14;
+			};
+    };
+  };
+
+}
